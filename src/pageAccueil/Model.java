517a316fb335d9f55c529129b3e482cb5a96a440
TomRemond <remondtom@gmail.com>
2019-03-21 09:43:33 +0100

    test

--- src/pageAccueil/Model.java
+++ src/pageAccueil/Model.java
@@ -1,5 +1,54 @@
 package pageAccueil;
 
-public class Model {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class Model {
+	
+	public Model() {
+		
+	}
+	
+	public static void Connexionbdd() {
+		/* Connexion à la base de données */
+		String url = "jdbc:mysql://localhost:3306/tp17tapas";
+		String utilisateur = "root";
+		String motDePasse = "";
+		Connection connexion = null;
+		Statement state = null;
+		
+		try {
+			
+		    connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+	
+		    /* Ici, nous placerons nos requêtes vers la BDD */
+		    state = connexion.createStatement();
+		    
+		    ResultSet resultat = state.executeQuery("");
+		    
+		    
+		    ResultSetMetaData resultMeta = resultat.getMetaData();	
+		    while(resultat.next()) {
+		    	for (int i = 1; i <= resultMeta.getColumnCount(); i++) {
+		    		System.out.println(resultat.getObject(i).toString());
+		    	}
+		    }
+		    resultat.close();
+	
+		} catch ( SQLException e ) {
+		    /* Gérer les éventuelles erreurs ici */
+		} finally {
+		    if ( connexion != null )
+		        try {
+		            /* Fermeture de la connexion */
+		            connexion.close();
+		        } catch ( SQLException ignore ) {
+		            /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
+		        }
+		}
+	}
 }
