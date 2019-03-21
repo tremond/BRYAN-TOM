c80d0eca4f71ee65a9851c22d8cdc8cbcb097aab
Warryan <Rudelin10222@jbdelasalle.com>
2019-03-21 08:59:34 +0100

    debutaccueil

---
+++ src/pageAccueil/MainApplication.java
@@ -0,0 +1,29 @@
+package pageAccueil;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application
+{
+	public void start(Stage primaryStage)
+	{
+		View vue = new View();
+		Model mdl = new Model();
+		
+		vue.init(mdl);
+
+		Scene scene = new Scene(vue, Color.WHITE);
+		primaryStage.setTitle("Tapas");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+
+		primaryStage.show();
+	}
+
+    public static void main(String[] args)
+    {
+    	Application.launch(MainApplication.class);
+    }
+}
