cf0fc4f7198da94b5f0dd7d3883be537cb85877b
Warryan <Rudelin10222@jbdelasalle.com>
2019-03-28 08:24:33 +0100

    zvmljzbvjl

--- src/pagePrincipale/Model.java
+++ src/pagePrincipale/Model.java
@@ -1,6 +1,19 @@
 package pagePrincipale;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Model 
 {
-
+	public static void connexionBdd() throws SQLException
+	{
+		String url = "jdbc:mysql://localhost:3306/tp17tapas";
+		String utilisateur = "root";
+		String motDePasse = "";
+		Connection connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+		
+		Statement state = connexion.createStatement();
+	}
 }
