e381af5b8a9eaa6e2e23d218f87b2c513484a1c7
Warryan <Rudelin10222@jbdelasalle.com>
2019-04-29 08:39:25 +0200

    salu

--- src/HistoriqueCommande/VueHistorique.java
+++ src/HistoriqueCommande/VueHistorique.java
@@ -1,8 +1,23 @@
 package HistoriqueCommande;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javafx.scene.control.ScrollPane;
+import javafx.scene.text.Text;
+import tools.ConnexionBdd;
 
 public class VueHistorique extends ScrollPane 
 {
-	
+	public VueHistorique() {
+		Statement state;
+		
+		try {
+		state = ConnexionBdd.getConnexion().createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Text title = new Text("Historique de commandes");
+		
+	}	
 }
